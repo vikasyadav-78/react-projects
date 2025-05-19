4e1a02346600f85cfe98955cb180e16764ddb378
yadavvikas787840@gmail.com <yadavvikas787840@gmail.com>
2025-05-19 12:26:20 +0530

    total amount set

--- Third Project/src/Components/Cart.jsx
+++ Third Project/src/Components/Cart.jsx
@@ -9,21 +9,26 @@ export default function Cart(props) {
         props.setAdd(updateCard)
     }
 
-
-    
+    const totalAmount = props.add.reduce((pre, cur) => pre + cur.price, 0)
 
     return (
         <>
-            <div className='flex flex-col gap-20 border-b  w-100 max-h-85 bg-white overflow-y-auto no-scrollbar relative'>
+            <div className='flex flex-col gap-20 border-b h-100 w-100 max-h-85 bg-white overflow-y-auto no-scrollbar relative'>
                 {props.add.map((items) => <div>
                     <div className='flex justify-between items-center p-3'>
-                        <img className='object-cover' width={"50%"} src={items.img} alt="" />
-                        <h1 className='font-bold'>{items.name}</h1>
+                        <div> 
+                            <img className='object-cover' width={"50%"} src={items.img} alt="" />
+                            <h1 className='font-bold text-start mt-3'>{items.name}</h1>
+                        </div>
                         <h1 className='font-bold'>{items.price}</h1>
                         <button onClick={() => removeItem(items.id)} className='font-bold border p-1 px-3 buttonCart'>X</button>
                     </div>
                 </div>
                 )}
+                <div className='flex gap-7  border-t-1 py-2'>
+                    <h1 className='font-bold'>Total : </h1>
+                    <p className='font-bold'> {totalAmount}</p>
+                </div>
             </div>
         </>
     )
