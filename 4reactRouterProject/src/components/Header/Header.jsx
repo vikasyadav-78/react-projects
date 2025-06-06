21942d663dbd631045839006cf8cfdab4aa2888c
yadavvikas787840@gmail.com <yadavvikas787840@gmail.com>
2025-06-06 15:40:53 +0530

    update navbar

--- 4reactRouterProject/src/components/Header/Header.jsx
+++ 4reactRouterProject/src/components/Header/Header.jsx
@@ -12,85 +12,28 @@ export default function Header() {
                             <img src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png" className="mr-3 h-12" alt="Logo" />
                         </Link>
                         <div className="flex items-center lg:order-2">
-                            <Link to="#" className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"> Log in
-                            </Link>
-                            <Link
-                                to="#"
-                                className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
-                            >
-                                Get started
-                            </Link>
+                            <Link to="#" className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none">Log in</Link>
+                            <Link to="#" className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none" >Get started </Link>
                         </div>
-                        <div
-                            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-                            id="mobile-menu-2"
-                        >
+                        <div className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1" id="mobile-menu-2">
                             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                                 <li>
-                                    <NavLink
-                                        to="/"
-                                        className={({ isActive }) =>
-                                            `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                        }
-                                    >
-                                        Home
-                                    </NavLink>
+                                    <NavLink to="/" className={({ isActive }) =>  `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0` }> Home </NavLink>
                                 </li>
                                 <li>
-                                    <NavLink
-                                        to="/about"
-                                        className={({ isActive }) =>
-                                            `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                        }
-                                    >
-                                        About
-                                    </NavLink>
+                                    <NavLink to="/about" className={({ isActive }) => `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0` }> About </NavLink>
                                 </li>
                                 <li>
-                                    <NavLink
-                                        to="/contact"
-                                        className={({ isActive }) =>
-                                            `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                        }
-                                    >
-                                        Contact
-                                    </NavLink>
+                                    <NavLink to="/contact" className={({ isActive }) => `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0` }> Contact </NavLink>
                                 </li>
                                 <li>
-                                    <NavLink
-                                        to="/github"
-                                        className={({ isActive }) =>
-                                            `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
-                                        }
-                                    >
-                                        Github
-                                    </NavLink>
+                                    <NavLink to="/github" className={({ isActive }) =>`block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0` }> Github </NavLink>
                                 </li>
-
                             </ul>
                         </div>
                     </div>
                 </nav>
             </header>
-            <header>
-                <nav className='px-5 py-2.5'>
-                    <div className='flex justify-between items-center'>
-                        <Link to={'#'}>
-                            <img width={"12%"} src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png" alt="Logo" />
-                        </Link>
-                        <div className='flex gap-10'>
-                            <Link className='px-4 py-2.5 font-medium text-smmr-2'  to={'/'}>Login </Link>
-                            <Link className='bg-orange-700 px-4 py-2 text-white font-medium text-sm rounded-lg mr-2' to={'/'}>Get Started </Link>
-                        </div>
-                        
-                        <div>
-                            
-                        </div>
-
-                    </div>
-                </nav>
-            </header>
         </>
-
     );
 }
