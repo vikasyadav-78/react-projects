bd81870e9ba8d7d1bbfbd0bd98c9f1434182e73e
yadavvikas787840@gmail.com <yadavvikas787840@gmail.com>
2025-06-09 12:19:48 +0530

    Footer updated and react icon install

--- 4reactRouterProject/src/App.jsx
+++ 4reactRouterProject/src/App.jsx
@@ -6,8 +6,8 @@ import Footer from './components/Footer/Footer'
 function App() {
   return (
     <>
-      <Header />
-      {/* <Footer /> */}
+      {/* <Header /> */}
+      <Footer />
     </>
   )
 }
