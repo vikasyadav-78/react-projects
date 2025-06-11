39c10ee12f8e5283ef4fc1ca7474df015fac9fd0
yadavvikas787840@gmail.com <yadavvikas787840@gmail.com>
2025-06-11 12:56:37 +0530

    Home updated and changes

--- 4reactRouterProject/src/components/Home/Home.jsx
+++ 4reactRouterProject/src/components/Home/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { IoLogoGooglePlaystore } from "react-icons/io5";
-import { img1 } from './index.js';
+import { img1, img2 } from './index.js';
 
 
 export default function Home() {
     return (
         <div className="mx-auto w-full max-w-7xl">
             <div className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
+                <div className="absolute w-full sm:my-20 sm:pt-1 pt-12 h-full ">
+                    <img className="w-96" src={img1} alt="image1" />
+                </div>
                 <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
                     <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
                         <h2 className="text-4xl font-bold sm:text-5xl"> Download Now
@@ -20,13 +23,24 @@ export default function Home() {
                     </div>
                 </div>
 
-                <div className="absolute inset-0 w-full sm:my-20 sm:pt-1 pt-12 h-full ">
-                    <img className="w-96" src={img1} alt="image1" />
+
+            </div>
+
+            <div className='flex justify-around items-center flex-col md:flex-row md:space-y-0 space-y-15'>
+                <div>
+                    <img className='w-96' src={img1} alt="" />
+                </div>
+                <div className='text-right space-y-5'>
+                    <div>
+                        <h1 className='whitespace-nowrap text-5xl font-bold lg:text-5xl sm:text-4xl'>Download Now </h1>
+                        <h2 className='text-4xl md:block hidden font-bold'>Lorem Ipsum</h2>
+                    </div>
+                    <Link to='/' className='inline-flex fw-bold rounded-lg font-medium bg-amber-700 px-6 py-3 items-center justify-center text-white hover:opacity-75'><IoLogoGooglePlaystore className='text-2xl' /> &nbsp; Download Now</Link>
                 </div>
             </div>
 
             <div className="grid  place-items-center sm:mt-20">
-                <img className="sm:w-96 w-48" src="https://i.ibb.co/2M7rtLk/Remote1.png" alt="image2" />
+                <img className="sm:w-96 w-48" src={img2} alt="image2" />
             </div>
 
             <h1 className="text-center text-2xl sm:text-5xl py-10 font-medium">Lorem Ipsum Yojo</h1>
--- 4reactRouterProject/src/components/Home/index.js
+++ 4reactRouterProject/src/components/Home/index.js
@@ -1,4 +1,4 @@
 import img1 from './Images/remote2.png'
+import img2 from './Images/Remote1-1-800x465.png'
 
-
-export {img1};
+export {img1 , img2};
