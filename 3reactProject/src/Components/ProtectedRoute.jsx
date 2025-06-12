a8d55836f4b0208b79d1bdc15211cba52ef2d08d
yadavvikas787840@gmail.com <yadavvikas787840@gmail.com>
2025-06-12 15:33:09 +0530

    changes

--- 3reactProject/src/Components/ProtectedRoute.jsx
+++ 3reactProject/src/Components/ProtectedRoute.jsx
@@ -1,6 +1,8 @@
 import { Navigate } from 'react-router-dom'
 
 function ProtectedRoute({ children }) {
+
+    
     let protect  = JSON.parse(localStorage.getItem('token'));
   if (!protect) {
     return <Navigate to="/login" />
