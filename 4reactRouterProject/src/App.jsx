018a66ff3ba70d8262d87550bbf8370d6a1cd80f
yadavvikas787840@gmail.com <yadavvikas787840@gmail.com>
2025-07-09 11:53:44 +0530

    add about and contact pages

--- 4reactRouterProject/src/App.jsx
+++ 4reactRouterProject/src/App.jsx
@@ -3,13 +3,37 @@ import './App.css'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
+import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+import Layout from './Layout'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children:[
+        {
+          path:'',
+          element:<Home />
+        },
+        {
+          path: 'about',
+          element:<About />
+        },
+        {
+          path: 'contact',
+          element:<Contact />
+        }
+      ]
+    }
+  ])
+
   return (
     <>
-      <Header />
-      <Home />
-      <Footer />
+      <RouterProvider router={router} />
     </>
   )
 }
